dad587814dbf15850d1c2314a2a569c6886936b4
Arunapindra <150082622+Arunapindra@users.noreply.github.com>
2024-10-06 16:52:38 -0500

    functions

--- mysql.sh
+++ mysql.sh
@@ -47,5 +47,12 @@ VALIDATE $? "Enabled mySQL server"
 systemctl start mysqld &>>$LOG_FILE
 VALIDATE $? "Started mySQL server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE
-VALIDATE $? "Setting up root password"
+mysql -h mysql.arudevops.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting uo root password"
+else
+    echo "MySQL is already setup...$Y SKIPPING $N" | tee -a $LOG_FILE
+fi
